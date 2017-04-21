670b90b3ff1f2b973e36705924140a56b1746a56
Nimesh <nimesh@Nimeshs-MacBook-Pro.local>
2017-04-21 11:35:58 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,31 +1,26 @@
 console.log("Up and running!");
 
-/*
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
-*/ // Used previously in a non array
-
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
+	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
-		alert("Sorry, try again.")
+		alert("Sorry, try again.");
 	}
 }
 
+var flipCard = function (cardID) {
+	
+	console.log("User flipped " + cards[cardID])
+	
+	cardsInPlay.push(cards[cardID]);
+	
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
